df452bbe2540895884602cba1d026afa6ae15ec0
DioneJa <brunoalves2017@gmail.com>
2022-09-14 16:17:55 -0300

    Sellers class

--- src/models/entities/Seller.java
+++ src/models/entities/Seller.java
@@ -1,5 +1,99 @@
 package models.entities;
 
-public class Seller {
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
 
+public class Seller implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String name;
+	private String email;
+	private LocalDate birthDate;
+	private Double baseSalary;
+
+	private Department dep;
+
+	public Seller() {
+
+	}
+
+	public Seller(Integer id, String name, String email, LocalDate birthDate, Double baseSalary, Department dep) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.birthDate = birthDate;
+		this.baseSalary = baseSalary;
+		this.dep = dep;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Double getBaseSalary() {
+		return baseSalary;
+	}
+
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
+	public Department getDep() {
+		return dep;
+	}
+
+	public void setDep(Department dep) {
+		this.dep = dep;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return id + ", " + name + ", " + email + ", " + birthDate + ", " + String.format("%.2f", baseSalary);
+	}
 }
